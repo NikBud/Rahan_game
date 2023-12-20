f307663945bdf5ccaa8c5c2c982862ea3214c7ab
NikBud <nikbud03@gmail.com>
2023-12-20 23:33:42 +0100

    Code was refactored and now it is more readable.

--- test.c
+++ test.c
@@ -8,15 +8,16 @@ int main(void){
     int i;
     Map* m = create_map();
     m->size = 10;
-    printf("is dead: %i \n", isDead(m->hero));
-    game_start(m->hero, m);
+    
+    render_map(m);
     for (i =0; i<2;i++){
         printf("Food: %s  Desc: %s\n", m->food[i].name, m->food[i].description);
         printf("Item: %s  Bonus: %d\n", m->items[i].description, m->items[i].stat_bonus);
     }
-    
-    printf("symbol: %c; current_hp: %d; pos_x: %d; pos_y: %d;\n", m->hero->symbol, m->hero->current_hp, m->hero->pos->x, m->hero->pos->y);
-    
+        
+    for(i = 0; i < m->position_list_size; i++){
+        printf("pos_x: %d; pos_y: %d\n", m->positions[i].x, m->positions[i].y);
+    }
 
     return 0;
 }
