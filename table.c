b23bc8bc87deb3739151a7a3349ac519cf3b34a8
Vladyslav Vasiliev <basedaccount@Vladyslavs-MacBook-Pro.local>
2023-12-20 13:42:04 +0100

    Implemented create items method and create food method

--- table.c
+++ table.c
@@ -92,7 +92,6 @@ void create_rocks(Map* map, Position* position_list, int position_list_size, int
 
 Monter generate_monster(char* name, int dmg, int hp, Position* position_list, int position_list_size){
     Monter final;
-    int i;
     create_unique_pos(position_list, position_list_size);
     final.damage = dmg;
     final.hp = hp;
@@ -101,15 +100,64 @@ Monter generate_monster(char* name, int dmg, int hp, Position* position_list, in
     final.pos = &position_list[position_list_size];
     return final;
 }
-char* get_name(){
-    int r;
+Food generate_food(char* name, char* description, int heal, Position* position_list, int position_list_size){
+    Food final;
+    create_unique_pos(position_list, position_list_size);
+    final.heal = rand() % 30;
+    final.name = name;
+    final.description = description;
+    final.symbol = position_list[position_list_size].symbol = '*';
+    final.pos = &position_list[position_list_size];
+    return final;
+}
+Item generate_item(char* description, int bonus, Item_Type type, Position* position_list, int position_list_size){
+    Item final;
+    create_unique_pos(position_list, position_list_size);
+    final.description = description;
+    final.stat_bonus = bonus;
+    final.type = type;
+    final.symbol = position_list[position_list_size].symbol = '!';
+    final.pos = &position_list[position_list_size];
+    return final;
+}
+char* get_food_desc(int rando){
     int counter = 0;
     char * line = malloc(sizeof(char) *100);
     FILE*fptr;
-    fptr = fopen("monsternames.txt", "r");
-    r = rand() % 17;
+    fptr = fopen("foodesc.txt", "r");
     while (fgets(line, 100, fptr) != NULL){
-            if (counter == r){
+            if (counter == rando){
+                printf("generated: %s\n", line);
+                fclose(fptr);
+                return line;
+            }
+            else{
+                counter++;
+            }
+    }
+    fclose(fptr);
+    return NULL;
+}
+char* get_name(int i, int rando){
+    int counter = 0;
+    char * line = malloc(sizeof(char) *100);
+    FILE*fptr;
+    switch(i){
+        case 1:
+        fptr = fopen("monsternames.txt", "r");
+        break;
+        case 2:
+        fptr = fopen("foodnames.txt", "r");
+        break;
+        case 3:
+        fptr = fopen("itemnames.txt", "r");
+        break;
+        default:
+        fptr = fopen("monsternames.txt", "r");
+        break;
+    }
+    while (fgets(line, 100, fptr) != NULL){
+            if (counter == rando){
                 printf("generated: %s\n", line);
                 fclose(fptr);
                 return line;
@@ -124,15 +172,62 @@ char* get_name(){
 void create_monsters(Map* m, Position* position_list, int position_list_size, int count){
     Monter* m1;
     int i;
+    int r;
     m1 = malloc(sizeof(Monter) * count);
     for (i = 0; i<count; i++){
-        m1[i] = generate_monster(get_name(), rand()%20, rand()%50, position_list, position_list_size);
+        r = rand() % 100;
+        m1[i] = generate_monster(get_name(1, r), rand()%20, rand()%50, position_list, position_list_size);
         position_list[position_list_size++].obj = &m1[i];    
     }
 
     m->monsters = m1;
 }
-    
+void create_food(Map* m, Position* position_list, int position_list_size, int count){
+    Food* f1;
+    int i;
+    int r;
+    f1 = malloc(sizeof(Food) * count);
+    for (i = 0; i<count; i++){
+        r = rand() % 10;
+        f1[i] = generate_food(get_name(2, r), get_food_desc(r), r*2, position_list, position_list_size);
+        position_list[position_list_size++].obj = &f1[i];    
+    }
+    m->food = f1;
+}
+void create_items(Map* m, Position* position_list, int position_list_size, int count){
+    Item* i1;
+    int i;
+    int r;
+    int bonus;
+    int cycle = 1;
+    i1 = malloc(sizeof(Item) * count);
+    for (i = 0; i<count; i++){
+        cycle = (i+1)%4;
+        r = rand() % 25;
+        bonus = rand() % 5;
+        printf("%i\n", cycle);
+        printf("Rand = %i\n", r);
+        switch(cycle){
+            case 1:
+            i1[i] = generate_item(get_name(3, 2*r), bonus+1, Protections, position_list, position_list_size);
+            position_list[position_list_size++].obj = &i1[i];  
+            break;
+            case 2:
+            i1[i] = generate_item(get_name(3, 50 + r), bonus+1, Armes, position_list, position_list_size);
+            position_list[position_list_size++].obj = &i1[i];  
+            break;
+            case 3:
+            i1[i] = generate_item(get_name(3, 75+r), bonus+1, Chaussures, position_list, position_list_size);
+            position_list[position_list_size++].obj = &i1[i];  
+            break;
+            default:
+            i1[i] = generate_item(get_name(3, 2*r), bonus, Protections, position_list, position_list_size);
+            position_list[position_list_size++].obj = &i1[i];  
+            break;
+        }
+    }
+    m->items = i1;
+}
 
 
 
@@ -149,8 +244,13 @@ Map* create_map() {
 
     create_rocks(game_map, position_list, pos_list_size, 4);
     pos_list_size = 4;
-    create_monsters(game_map, position_list, pos_list_size, 1);
-    pos_list_size++;
+    create_monsters(game_map, position_list, pos_list_size, 3);
+    pos_list_size+=3;
+    create_food(game_map, position_list, pos_list_size, 2);
+    pos_list_size+=2;
+    create_items(game_map, position_list, pos_list_size, 2);
+    pos_list_size+=2;
+
     
 
     game_map->positions = position_list;
